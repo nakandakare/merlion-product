ead43bb4079d800d54994bc1a530a083a9aced13
Kevin <kevin.nakandakare@gmail.com>
2020-08-16 16:34:31 -0300

    Auto generated code

---
+++ src/main/webapp/app/shared/layout/menus/entities.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import MenuItem from 'app/shared/layout/menus/menu-item';
+import { DropdownItem } from 'reactstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Translate, translate } from 'react-jhipster';
+import { NavLink as Link } from 'react-router-dom';
+import { NavDropdown } from './menu-components';
+
+export const EntitiesMenu = props => (
+  <NavDropdown
+    icon="th-list"
+    name={translate('global.menu.entities.main')}
+    id="entity-menu"
+    style={{ maxHeight: '80vh', overflow: 'auto' }}
+  >
+    <MenuItem icon="asterisk" to="/product">
+      <Translate contentKey="global.menu.entities.product" />
+    </MenuItem>
+    <MenuItem icon="asterisk" to="/user-with-perms">
+      <Translate contentKey="global.menu.entities.userWithPerms" />
+    </MenuItem>
+    <MenuItem icon="asterisk" to="/permissions">
+      <Translate contentKey="global.menu.entities.permissions" />
+    </MenuItem>
+    {/* jhipster-needle-add-entity-to-menu - JHipster will add entities to the menu here */}
+  </NavDropdown>
+);
